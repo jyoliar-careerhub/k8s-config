8e274e57990c1b52e78af2c029f12aa9dd7394ea
jyo liar <jyo-liar@gmail.com>
2025-04-13 19:18:01 +0900

    gateway 생성

---
+++ init/gateway/init.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -euo pipefail
+
+init_config_paths="
+gateway-crds/
+aws-loadbalancer-controller/
+gateway-controller/
+gateway/
+"
+
+for path in $init_config_paths; do
+    echo "Checking for changes in $path..."
+    IS_DIFF=$(kubectl diff -k "$path" 2>&1 || true)
+
+    if [ -n "$IS_DIFF" ]; then
+        echo "Changes detected in $path. Applying changes..."
+        kubectl apply -k "$path" | grep -v "unchanged"
+        echo "Successfully applied changes in $path."
+        sleep 3
+    else
+        echo "No changes detected in $path."
+    fi
+done
