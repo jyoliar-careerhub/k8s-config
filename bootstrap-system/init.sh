674623628d98cf857181577d0169be4840fc384e
jyo liar <jyo-liar@gmail.com>
2025-04-22 14:45:31 +0900

    init.sh 오류 수정

--- bootstrap-system/init.sh
+++ bootstrap-system/init.sh
@@ -16,7 +16,12 @@ for path in $init_config_paths; do
         echo "Changes detected in $path. Applying changes..."
         kubectl apply -k "$path" | grep -v "unchanged"
         kubectl wait --for=condition=available --timeout=60s --all deployments -A > /dev/null
-        kubectl wait --for=jsonpath='{.status.health.status}'=Healthy --timeout 60s --all app -A > /dev/null
+
+        IS_APPS=$(kubectl get application -A --no-headers | wc -l)
+        if [ "$IS_APPS" -gt 0 ]; then
+            kubectl wait --for=jsonpath='{.status.health.status}'=Healthy --timeout 80s --all app -A > /dev/null
+        fi
+        
         echo "Successfully applied changes in $path."
 
         sleep 5
